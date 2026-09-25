e7f1f913573c9e0aad2fd5ce09fe9fbc65cba451
Isaac <isaac052503@gmail.com>
2026-09-24 21:58:54 -0300

    feat: scripts de graph state
    obs: será aberta uma branch de integração para finalizar as coisas de setembro

---
+++ api/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .state import HeroState, MonsterState, NPCState, Message, GameState                                                                                  
+                                                                                                                                                                
+__all__ = [                                                                                                                                                 
+    "HeroState",                                                                                                                                            
+    "MonsterState",
+    "NPCState",
+    "Message",
+    "GameState",
+]
